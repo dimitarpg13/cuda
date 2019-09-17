fab88d378cd4061f13b82d6bd6f947dec448cbcb
Dimitar Gueorguiev <dimitar.gueorguiev@celect.com>
2019-09-17 12:54:26 -0600

    re/moving solution and project files

---
+++ CUDA_Samples/v10.1/work/thrust_vector_container_ex1/thrust_vector_container_ex1.cu
@@ -0,0 +1,42 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 1993-2015 NVIDIA Corporation.  All rights reserved.
+//
+// Please refer to the NVIDIA end user license agreement (EULA) associated
+// with this source code for terms and conditions that govern your use of
+// this software. Any use, reproduction, disclosure, or distribution of
+// this software and related documentation outside the terms of the EULA
+// is strictly prohibited.
+//
+////////////////////////////////////////////////////////////////////////////
+
+/* Template project which demonstrates the basics on how to setup a project
+* example application.
+* Host code.
+*/
+
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/generate.h>
+#include <thrust/sort.h>
+#include <thrust/copy.h>
+#include <algorithm>
+#include <cstdlib>
+
+int main(void)
+{
+	// generate 32M random numbers serially
+	thrust::host_vector<int> h_vec(32 << 20);
+	std::generate(h_vec.begin(), h_vec.end(), rand);
+
+	// transfer data to the device
+	thrust::device_vector<int> d_vec = h_vec;
+
+	// sort data on the device (846M keys per second on GeForce GTX 480)
+	thrust::sort(d_vec.begin(), d_vec.end());
+
+	// transfer data back to host
+	thrust::copy(d_vec.begin(), d_vec.end(), h_vec.begin());
+
+	return 0;
+}
