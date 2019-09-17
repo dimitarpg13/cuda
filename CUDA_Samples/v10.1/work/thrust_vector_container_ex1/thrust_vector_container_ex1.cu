f1a70c2136db10768fc7a236d320c3723e9c4a3b
Dimitar Gueorguiev <dimitar.gueorguiev@celect.com>
2019-09-17 13:16:00 -0600

    more thrust examples

--- CUDA_Samples/v10.1/work/thrust_vector_container_ex1/thrust_vector_container_ex1.cu
+++ CUDA_Samples/v10.1/work/thrust_vector_container_ex1/thrust_vector_container_ex1.cu
@@ -17,26 +17,47 @@
 
 #include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
-#include <thrust/generate.h>
-#include <thrust/sort.h>
-#include <thrust/copy.h>
-#include <algorithm>
-#include <cstdlib>
+
+#include <iostream>
 
 int main(void)
 {
-	// generate 32M random numbers serially
-	thrust::host_vector<int> h_vec(32 << 20);
-	std::generate(h_vec.begin(), h_vec.end(), rand);
+	// H has storage for 4 integers
+	thrust::host_vector<int> H(4);
 
-	// transfer data to the device
-	thrust::device_vector<int> d_vec = h_vec;
+	// initialize individual elements
+	H[0] = 14;
+	H[1] = 20;
+	H[2] = 38;
+	H[3] = 46;
 
-	// sort data on the device (846M keys per second on GeForce GTX 480)
-	thrust::sort(d_vec.begin(), d_vec.end());
+	// H.size() returns the size of vector H
+	std::cout << "H has size " << H.size() << std::endl;
 
-	// transfer data back to host
-	thrust::copy(d_vec.begin(), d_vec.end(), h_vec.begin());
+	// print contents of H
+	for (int i = 0; i < H.size(); i++)
+	{
+		std::cout << "H[" << i << "] = " << H[i] << std::endl;
+	}
 
+	// resize H
+	H.resize(2);
+
+	std::cout << "H now has size " << H.size() << std::endl;
+
+	// Copy host_vector H to device_vector D
+	thrust::device_vector<int> D = H;
+
+	// elements of D can be modified
+	D[0] = 99;
+	D[1] = 88;
+
+	// print contents of D
+	for (int i = 0; i < D.size(); i++)
+	{
+		std::cout << "D[" << i << "] = " << D[i] << std::endl;
+	}
+
+	// H and D are automatically destroyed when the function returns
 	return 0;
 }
